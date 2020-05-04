d31d75fb850c68099924bcb084e5aa072e6b6d3b
yemulamanasa <49743181+yemulamanasa@users.noreply.github.com>
2020-05-04 11:10:09 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,20 +1,3 @@
-/*
-Solution of assignment 4:
-
-Expected output:
-
-Hello Yaakov
-Good Bye John
-Good Bye Jen
-Good Bye Jason
-Hello Paul
-Hello Frank
-Hello Larry
-Hello Paula
-Hello Laura
-Good Bye Jim
-*/
-
 (function() {
     var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
     for (var i = 0; i < names.length; i++) {
